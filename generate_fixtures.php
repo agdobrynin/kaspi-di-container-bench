9c065917bd0bd2f90e8193f74425580c309b5055
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-09 18:26:22 +0400

    Refactoring the fixture generator.

--- generate_fixtures.php
+++ generate_fixtures.php
@@ -5,7 +5,9 @@ $fixturesDir = [
     'interfaces' => __DIR__ . '/src/Services/Interfaces/',
 ];
 
-$classesCount = 1000;
+$classServiceNamePrefix = 'Service';
+$namespaceService = 'App\\Services';
+
 $tagSuffix = [
     'foo',
     'bar',
@@ -21,37 +23,54 @@ namespace App\Services\Interfaces;
 interface ServiceInterface {}
 ');
 
-for($i=0; $i < $classesCount; $i++) {
-    $autowire = '';
-    if (0 === (random_int(0, $classesCount) % 2)) {
+/*
+ * Make services
+ */
+
+$injectService = null;
+$countOfService = 1000;
+
+do {
+    $serviceShortName = $classServiceNamePrefix.$countOfService;
+
+    $autowireAttribute = '';
+    $implementInterface = '';
+
+    if (0 === (random_int(0, 100) % 2)) {
         $suffix = $tagSuffix[random_int(0, count($tagSuffix) - 1)];
         $tag = '\'tags.name_'.$suffix.'\'';
-        $autowire = <<< AUTOWIRE
+        $autowireAttribute = <<< AUTOWIRE
 use Kaspi\DiContainer\Attributes\{Autowire, Tag};
 
 #[Autowire(tags: new Tag($tag))]
 AUTOWIRE;
-        $interface = '';
     } else {
-        $interface = 'implements \App\Services\Interfaces\ServiceInterface';
+        $implementInterface = 'implements \App\Services\Interfaces\ServiceInterface';
     }
-
-    $serviceName = 'Service'.$i;
-    $diConfigure[] = 'App\Services\\'.$serviceName;
 
     $template = <<< TMPL
 <?php
 declare(strict_types=1);
 
-namespace App\Services;
-$autowire
-final class  $serviceName $interface
-{}
+namespace $namespaceService;
+$autowireAttribute
+final class $serviceShortName $implementInterface
+{
+    public function __construct($injectService) {}
+}
 
 TMPL;
-    file_put_contents($fixturesDir['services'].'/'.$serviceName.'.php', $template);
 
-}
+    $diConfigure[] = $namespaceService.'\\'.$serviceShortName;
+
+    if (null === $injectService) {
+        $injectService = 'public readonly \\'.$namespaceService.'\\'.$serviceShortName.' $service';
+    }
+
+    file_put_contents($fixturesDir['services'].'/'.$serviceShortName.'.php', $template);
+
+    $countOfService--;
+} while ($countOfService > 0);
 
 shuffle($diConfigure);
 
