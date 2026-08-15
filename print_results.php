63714a57fa7444943e29d4fd6e78ef4fb7bf4ec5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-15 11:18:50 +0400

    PHP script for print benchmark results.

---
+++ print_results.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use Kaspi\Benchmark\Config\Configuration;
+use Kaspi\Benchmark\Core\BenchmarkPrinter;
+use Kaspi\Benchmark\Core\BenchmarkResultsFile;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$file = new BenchmarkResultsFile(Configuration::JsonFileResult->getValue());
+$printer = new BenchmarkPrinter();
+
+foreach ($file->read() as $benchmarkResult) {
+    $printer->attach($benchmarkResult);
+}
+
+$printer->printEachGroup();
