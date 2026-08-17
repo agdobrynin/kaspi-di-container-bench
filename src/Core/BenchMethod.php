7e315fed3637951bedf4a08711f8ed0e2e762553
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-17 17:42:12 +0400

    Parameters for `BenchmarkMethod`.

--- src/Core/BenchMethod.php
+++ src/Core/BenchMethod.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kaspi\Benchmark\Core;
 
+use Generator;
 use ReflectionMethod;
 
 final class BenchMethod
@@ -12,6 +13,7 @@ final class BenchMethod
      * @param non-empty-string $description
      * @param list<ReflectionMethod> $beforeReflectionMethod methods called before the execution of the benchmark method
      * @param list<ReflectionMethod> $afterReflectionMethod methods called after the execution of the benchmark method
+     * @param list<callable(): Generator|array> $parameters parameters for benchmark method
      */
     public function __construct(
         public readonly string $description,
@@ -20,5 +22,6 @@ final class BenchMethod
         public readonly int $iterations = 1,
         public readonly array $beforeReflectionMethod = [],
         public readonly array $afterReflectionMethod = [],
+        public readonly array $parameters = [],
     ) {}
 }
