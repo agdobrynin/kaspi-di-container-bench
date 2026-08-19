18016cbf9eb0e9e75c0afbdfe06783b0645478e9
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-19 17:09:12 +0400

    Refactoring `BenchmarkPrinter`.
    * Report `BenchmarkPrinter::printEachVersion()` - table of benchmark results for each version container.
    
    Refactoring `BenchmarkPrinter`.
    
    * `BenchmarkPrinter::printEachVersion()` report: a table of benchmark results for each package version.
    
    * `BenchmarkPrinter::printCompareVersions()` report: a table of benchmark results grouped by benchmark group names, benchmark descriptions, and package versions.

--- src/Core/BenchmarkPrinter.php
+++ src/Core/BenchmarkPrinter.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Kaspi\Benchmark\Core;
 
+use function array_key_last;
+use function array_shift;
+use function count;
 use function explode;
-use function sprintf;
+use function printf;
 use function wordwrap;
-use const PHP_EOL;
 
 final class BenchmarkPrinter
 {
@@ -27,58 +29,44 @@ final class BenchmarkPrinter
         unset($this->benchmarkResultsCollection);
     }
 
-    public function tableHeader(): string
+    public function printEachVersion(): void
     {
-        $th = <<< TABLEHEAD
+        $this->collectionIsEmpty();
+
+        $currentPackageVersion = null;
+        $formatResult = "\n| %-3s | %-52s | %-5s | %-11s | %-11s | %-11s |";
+        $formatTableLineSeparator = "\n+%'-5s+%'-54s+%'-7s+%'-13s+%'-13s+%'-13s+";
+
+        printf($formatTableLineSeparator, '', '', '', '', '', '');
+
+        print <<< TABLEHEAD
 
 |     |                                                      |       |         Memory            |    Time     |
 | No. | Benchmark description                                | Iter. +-------------+-------------+  execution  |
 |     |                                                      |       |  Allocated  |    Peak     |             |
 TABLEHEAD;
 
-        return $this->tableLineSeparator() . $th . $this->tableLineSeparator();
-    }
+        printf($formatTableLineSeparator, '', '', '', '', '', '');
 
-    public function tableLineSeparator(): string
-    {
-        return <<< LINESE
-
-+-----+------------------------------------------------------+-------+-------------+-------------+-------------+
-LINESE;
-
-    }
-
-    public function printEachVersion(): void
-    {
-        if (!isset($this->benchmarkResultsCollection)) {
-            print "Benchmark results collection is empty.\n";
-
-            return;
-        }
-
-        print $this->tableHeader();
-
-        $currentPackageVersion = null;
 
         foreach ($this->benchmarkResultsCollection as $benchmarkResult) {
             if ($currentPackageVersion !== $benchmarkResult->packageVersion) {
-                print sprintf("\n| %-108.108s |", $benchmarkResult->packageVersion);
+                printf("\n| %-108s |", $benchmarkResult->packageVersion);
                 $currentPackageVersion = $benchmarkResult->packageVersion;
-                print sprintf("\n+%'-110s+", '');
+                printf("\n+%'-110s+", '');
             }
 
-            print sprintf("\n| %-108.108s |", $benchmarkResult->groupName);
-            print $this->tableLineSeparator();
+            printf("\n| %-108s |", $benchmarkResult->groupName);
+            printf($formatTableLineSeparator, '', '', '', '', '', '');
 
             $timeExecuteMemoryUsingSumItems =  $benchmarkResult->getTimeExecuteMemoryUsingSumItems();
 
             $n = 1;
-            $formatResult = PHP_EOL . '| %-3.3s | %-52.52s | %-5.5s | %-11.11s | %-11.11s | %-11.11s |';
 
             foreach ($timeExecuteMemoryUsingSumItems as $benchmarkDescription => $timeExecuteMemoryUsingSum) {
                 $description = explode("\n", wordwrap($benchmarkDescription, 52, cut_long_words: true));
 
-                print sprintf(
+                printf(
                     $formatResult,
                     $n,
                     $description[0],
@@ -89,14 +77,92 @@ LINESE;
                 );
 
                 for ($i = 1, $c = count($description); $i < $c; $i++) {
-                    print sprintf($formatResult, '', $description[$i], '', '', '', '');
+                    printf($formatResult, '', $description[$i], '', '', '', '');
                 }
 
-                print $this->tableLineSeparator();
+                printf($formatTableLineSeparator, '', '', '', '', '', '');
                 $n++;
             }
         }
 
         print "\n";
+    }
+
+    public function printCompareVersions(): void
+    {
+        $this->collectionIsEmpty();
+        $tableResults = [];
+
+        // collect results group by "benchmark group name", "benchmark description", "package version".
+        foreach ($this->benchmarkResultsCollection as $benchmarkResult) {
+            foreach ($benchmarkResult->getTimeExecuteMemoryUsingSumItems() as $benchmarkDescription => $timeExecuteMemoryUsingSum) {
+                $tableResults[$benchmarkResult->groupName][$benchmarkDescription][$benchmarkResult->packageVersion] = $timeExecuteMemoryUsingSum;
+            }
+        }
+
+        $formatGroup = "\n| %-108s |";
+        $formatResult = "\n| %45s | %10s | %-5s | %-11s | %-11s | %-11s |";
+        $formatDivResult = "\n| %45s +%'-12s+%'-7s+%'-13s+%'-13s+%'-13s+";
+        $formatLineDescription = "\n| %45s |%-12s|%-7s|%-13s|%-13s|%-13s|";
+        $formatLineBound = "\n+%'-47s+%'-12s+%'-7s+%'-13s+%'-13s+%'-13s+";
+
+        print <<< TABLEHEAD
++-----+-----------------------------------------+------------+-------+-------------+-------------+-------------+
+| Benchmarks group                              |    Package |       |         Memory            |    Time     |
+|  ↘️  Benchmark description                    |    version | Iter. +-------------+-------------+  execution  |
+|                                               |            |       |  Allocated  |    Peak     |             |
+TABLEHEAD;
+
+        printf($formatLineBound, '', '', '', '', '', '');
+
+        foreach ($tableResults as $groupName => $benchmarkResults) {
+            printf($formatGroup, $groupName);
+            printf($formatLineBound, '', '', '', '', '', '');
+
+            foreach ($benchmarkResults as $benchmarkDescription => $packageVersions) {
+                $descriptionWrap = explode("\n", wordwrap($benchmarkDescription, 45, cut_long_words: true));
+                $lastPackageVersion = array_key_last($packageVersions);
+
+                foreach ($packageVersions as $packageVersion => $timeExecuteMemoryUsingSum) {
+                    $descriptionWrapLine = array_shift($descriptionWrap);
+
+                    printf(
+                        $formatResult,
+                        $descriptionWrapLine,
+                        $packageVersion,
+                        $timeExecuteMemoryUsingSum->iterations,
+                        Formatter::formatBytes($timeExecuteMemoryUsingSum->memoryUsageUsage, 4),
+                        Formatter::formatBytes($timeExecuteMemoryUsingSum->memoryPeakUsage, 4),
+                        Formatter::formatTimeExecute($timeExecuteMemoryUsingSum->hrTime, 4),
+                    );
+
+                    if ($lastPackageVersion !== $packageVersion) {
+                        $descriptionWrapLine = array_shift($descriptionWrap);
+                        printf($formatDivResult, $descriptionWrapLine, '', '', '', '', '');
+                    }
+                }
+
+                do {
+                    $descriptionWrapLine = array_shift($descriptionWrap);
+                    if (null === $descriptionWrapLine) {
+                        printf($formatLineBound, '', '', '', '', '', '');
+                    } else {
+                        printf($formatLineDescription, $descriptionWrapLine, '', '', '', '', '');
+                    }
+                } while (null !== $descriptionWrapLine);
+
+            }
+        }
+
+        print "\n";
+    }
+
+    private function collectionIsEmpty(): void
+    {
+        if (!isset($this->benchmarkResultsCollection)) {
+            print "Benchmark results collection is empty.\n";
+
+            exit(1);
+        }
     }
 }
