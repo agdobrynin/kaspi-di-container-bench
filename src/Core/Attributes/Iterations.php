32de8835b17751fe4af722fefb7980c59d1fab04
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-20 21:06:32 +0400

    The `$iterations` argument of the `Iterations` attribute must be 1 or greater.

--- src/Core/Attributes/Iterations.php
+++ src/Core/Attributes/Iterations.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace Kaspi\Benchmark\Core\Attributes;
 
 use Attribute;
+use function max;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 final class Iterations
 {
-    public function __construct(public readonly int $iterations = 1) {}
+    public readonly int $iterations;
+    public function __construct(int $iterations = 1)
+    {
+        $this->iterations = max($iterations, 1);
+    }
 }
